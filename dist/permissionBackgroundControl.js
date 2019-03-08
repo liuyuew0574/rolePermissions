9d9d8fbcd36c4a1709a363c7475d117b3f5f18ca
刘月伟 <liuyw@7224.com>
2019-03-08 15:10:11 +0800

    获取权限js

---
+++ dist/permissionBackgroundControl.js
@@ -0,0 +1,77 @@
+let permissionBackground = {
+
+};
+
+permissionBackground.Overview = function () {
+  return $nuxt.$store.getters.loginInfo.permissions.indexOf('Overview')!=-1
+};
+permissionBackground.ProductManager = function () {
+  return $nuxt.$store.getters.loginInfo.permissions.indexOf('ProductManager')!=-1
+};
+permissionBackground.CustomerManager = function () {
+  return $nuxt.$store.getters.loginInfo.permissions.indexOf('CustomerManager')!=-1
+};
+permissionBackground.SeeBuyersList = function () {
+  return $nuxt.$store.getters.loginInfo.permissions.indexOf('SeeBuyersList')!=-1
+};
+permissionBackground.seeBuyerDetail = function () {
+  return $nuxt.$store.getters.loginInfo.permissions.indexOf('seeBuyerDetail')!=-1
+};
+permissionBackground.WriteFollow = function () {
+  return $nuxt.$store.getters.loginInfo.permissions.indexOf('WriteFollow')!=-1
+};
+permissionBackground.SupplierManager = function () {
+  return $nuxt.$store.getters.loginInfo.permissions.indexOf('SupplierManager')!=-1
+};
+permissionBackground.SeeSupplierList = function () {
+  return $nuxt.$store.getters.loginInfo.permissions.indexOf('SeeSupplierList')!=-1
+};
+permissionBackground.CreateSupplier = function () {
+  return $nuxt.$store.getters.loginInfo.permissions.indexOf('CreateSupplier')!=-1
+};
+permissionBackground.ImportSupplier = function () {
+  return $nuxt.$store.getters.loginInfo.permissions.indexOf('ImportSupplier')!=-1
+};
+permissionBackground.UpdateSupplier = function () {
+  return $nuxt.$store.getters.loginInfo.permissions.indexOf('UpdateSupplier')!=-1
+};
+permissionBackground.SeeSupplierDetail = function () {
+  return $nuxt.$store.getters.loginInfo.permissions.indexOf('SeeSupplierDetail')!=-1
+};
+permissionBackground.SeeSupplierApproval = function () {
+  return $nuxt.$store.getters.loginInfo.permissions.indexOf('SeeSupplierApproval')!=-1
+};
+permissionBackground.SupplierApprovalOperating = function () {
+  return $nuxt.$store.getters.loginInfo.permissions.indexOf('SupplierApprovalOperating')!=-1
+};
+permissionBackground.UserManager = function () {
+  return $nuxt.$store.getters.loginInfo.permissions.indexOf('UserManager')!=-1
+};
+permissionBackground.SeeUserList = function () {
+  return $nuxt.$store.getters.loginInfo.permissions.indexOf('SeeUserList')!=-1
+};
+permissionBackground.CreateUser = function () {
+  return $nuxt.$store.getters.loginInfo.permissions.indexOf('CreateUser')!=-1
+};
+permissionBackground.UpdateUser = function () {
+  return $nuxt.$store.getters.loginInfo.permissions.indexOf('UpdateUser')!=-1
+};
+permissionBackground.ResetPasswordUser = function () {
+  return $nuxt.$store.getters.loginInfo.permissions.indexOf('ResetPasswordUser')!=-1
+};
+permissionBackground.EnableUser = function () {
+  return $nuxt.$store.getters.loginInfo.permissions.indexOf('EnableUser')!=-1
+};
+permissionBackground.Transaction = function () {
+  return $nuxt.$store.getters.loginInfo.permissions.indexOf('Transaction')!=-1
+};
+permissionBackground.SettingManager = function () {
+  return $nuxt.$store.getters.loginInfo.permissions.indexOf('SettingManager')!=-1
+};
+permissionBackground.ArticleManager = function () {
+  return $nuxt.$store.getters.loginInfo.permissions.indexOf('ArticleManager')!=-1
+};
+permissionBackground.CorpAdmin = function () {
+  return $nuxt.$store.getters.loginInfo.permissions.indexOf('CorpAdmin')!=-1
+};
+export default permissionBackground;
---
+++ dist/permissionSupplierControl.js
@@ -0,0 +1,41 @@
+let permissionSupplier = {
+
+};
+
+permissionSupplier.ProductManager = function () {
+  return $nuxt.$store.getters.loginInfo.permissions.indexOf('ProductManager')!=-1
+};
+permissionSupplier.SpotProductManager = function () {
+  return $nuxt.$store.getters.loginInfo.permissions.indexOf('SpotProductManager')!=-1
+};
+permissionSupplier.AvailableProductManager = function () {
+  return $nuxt.$store.getters.loginInfo.permissions.indexOf('AvailableProductManager')!=-1
+};
+permissionSupplier.AddAvailableProduct = function () {
+  return $nuxt.$store.getters.loginInfo.permissions.indexOf('AddAvailableProduct')!=-1
+};
+permissionSupplier.RFQ = function () {
+  return $nuxt.$store.getters.loginInfo.permissions.indexOf('RFQ')!=-1
+};
+permissionSupplier.OrderManager = function () {
+  return $nuxt.$store.getters.loginInfo.permissions.indexOf('OrderManager')!=-1
+};
+permissionSupplier.CustomerManager = function () {
+  return $nuxt.$store.getters.loginInfo.permissions.indexOf('CustomerManager')!=-1
+};
+permissionSupplier.FundsManager = function () {
+  return $nuxt.$store.getters.loginInfo.permissions.indexOf('FundsManager')!=-1
+};
+permissionSupplier.AccountManager = function () {
+  return $nuxt.$store.getters.loginInfo.permissions.indexOf('AccountManager')!=-1
+};
+permissionSupplier.AccountSecurity = function () {
+  return $nuxt.$store.getters.loginInfo.permissions.indexOf('AccountSecurity')!=-1
+};
+permissionSupplier.AccountSettings = function () {
+  return $nuxt.$store.getters.loginInfo.permissions.indexOf('AccountSettings')!=-1
+};
+permissionSupplier.BankAccountManager = function () {
+  return $nuxt.$store.getters.loginInfo.permissions.indexOf('BankAccountManager')!=-1
+};
+export default permissionSupplier;
